40c92bfca7abdd6fb085144deb2972494550236c
FuChih <t19887348@hotmail.com>
2021-10-27 10:07:05 +0800

    Item hover

--- src/components/List/Item/index.jsx
+++ src/components/List/Item/index.jsx
@@ -2,14 +2,23 @@ import React, { Component } from 'react'
 import './index.module.css'
 
 export default class Item extends Component {
+  state = { mouse: false }
+
+  mouse = (flag) => {
+    return () => {
+      this.setState({ mouse: flag })
+    }
+  }
+
   render() {
     const { name, done } = this.props
+    const { mouse } = this.state
 
     return (
-      <li>
+      <li onMouseEnter={this.mouse(true)} onMouseLeave={this.mouse(false)} style={{ backgroundColor: mouse ? '#fff' : '#ccc' }}>
         <input type="checkbox" defaultChecked={done} />
         <span>{name}</span>
-        <button>Delete</button>
+        <button style={{ display: mouse ? 'block' : 'none' }}>Delete</button>
       </li>
     )
   }
